d53b5e754cb4346c1ced84f477c8d513ebb16f8d
samtha83 <samtha.learn@gmail.com>
2025-04-23 16:52:50 -0400

    app fixed

--- src/app.py
+++ src/app.py
@@ -26,4 +26,4 @@ def predict():
     return jsonify({'predictions': predictions}) 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True,port=5000, host='0.0.0.0')
